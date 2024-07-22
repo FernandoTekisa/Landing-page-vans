24d702aadb6b36096eaf03ccd30ba58a0476f33a
Fernando Tekisa <164080430+FernandoTekisa@users.noreply.github.com>
2024-07-22 02:24:19 +0100

    Update Background.tsx

--- app/components/Background.tsx
+++ app/components/Background.tsx
@@ -5,17 +5,8 @@ export default function Background() {
         <section className=" flex justify-center items-center  bg-vans h-72 mt-24 ">
             <Conteiner>
                 <h1 className="text-6xl">Vans</h1>
-                <h2 className="text-5xl">"OFF THE WALL"</h2>
+                <h2 className="text-5xl">OFF THE WALL</h2>
             </Conteiner>
         </section>
     )
 }
-
-{/* <div className="">
-
-</section>
-<div className="mx-7 mt-5">
-
-
-</div>
-</div> */}
